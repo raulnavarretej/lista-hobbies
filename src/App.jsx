a54161fde256dc4a2d5748e91d8df90ec8f727ee
Raul Navarrete Jimenez <iscla19350310@gmail.com>
2023-03-01 16:04:42 -0600

    lista-hobbies

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import Footer from './Footer';
+function App(){
+  const name = "Raul"
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <h3>{name}'s hobbies list</h3>
+      <ul>
+        <li>Play video games</li>
+        <li>Reading</li>
+        <li>Play guitar</li>
+      </ul>
+      <hr />
+      <Footer />    
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
   )
+
 }
 
 export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,10 @@
+const Footer =() => {
+    return(
+        <div>
+            Thankyou for read my <b>hobbies list</b>
+        </div>
+    )
+
+}
+
+export default Footer;
